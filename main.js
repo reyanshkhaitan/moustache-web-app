99e8aeda7d8f589afab3cdf1dfa5e0245f1f3861
reyanshkhaitan <77532864+reyanshkhaitan@users.noreply.github.com>
2021-07-30 15:44:26 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,21 @@
+function preload() { 
+    //clownNose = loadImage('https://i.postimg.cc/7ZBcjDqp/clownnose.png');
+}
+
+function setup() {
+    canvas = createCanvas(300, 300);
+    canvas.center();
+    video = createCapture(VIDEO);
+    video.size(300, 300);
+    video.hide();
+    //poseNet = ml5.poseNet(video, modelLoaded);
+    //poseNet.on('pose', getPoses);
+}
+
+function draw() {
+    image(video, 0, 0, 300, 300);
+}
+
+function take_snapshot(){
+    save('moustache.png');
+}
